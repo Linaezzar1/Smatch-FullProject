702183821003c70083e48c89f272bdf528afe436
Linaezzar1 <ezzarlina@gmail.com>
2025-11-06 22:05:09 +0100

    Remove embedded repo entries

--- smatch-backend-main/models/Coach.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const coachSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  licenseNumber: String
-});
-
-module.exports = mongoose.model('Coach', coachSchema);
